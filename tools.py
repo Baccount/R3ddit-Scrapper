ff0aee65d23c91c923ac30762004aa6ee50d5b0d
“Brandon <cassias.long0r@icloud.com>
2022-08-24 12:30:35 -0700

    fixes to function docs

--- tools.py
+++ tools.py
@@ -7,33 +7,21 @@ from pyfiglet import Figlet
 
 def blue(text: str) -> str:
     """
-    `blue` takes a string and returns a string
-
-    :param text: The text to be colored
-    :type text: str
-    :return: The text is being returned with the color blue.
+    `blue` takes a string and returns a blue string
     """
     return "\033[34m" + text + "\033[0m"
 
 
 def green(text: str) -> str:
     """
-    `green` takes a string and returns a string
-
-    :param text: The text to be colored
-    :type text: str
-    :return: The text is being returned with the color green.
+    `green` takes a string and returns a green string
     """
     return "\033[32m" + text + "\033[0m"
 
 
 def red(text: str) -> str:
     """
-    `red` takes a string and returns a string
-
-    :param text: The text to be colored
-    :type text: str
-    :return: The text is being returned with the color red.
+    `red` takes a string and returns a red string
     """
     return "\033[31m" + text + "\033[0m"
 
