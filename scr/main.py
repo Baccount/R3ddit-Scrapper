dafcd0cf16396fa68b0b8424a90eeb1fe84e95e9
“Brandon <cassias.long0r@icloud.com>
2022-09-22 07:04:27 -0700

    bump version and small bug fix

--- scr/main.py
+++ scr/main.py
@@ -2,14 +2,12 @@ from arguments.arguments import argument
 from classes.main_class import R3dditScrapper
 from functions import create_config, getInput, showSplash
 
-VERSION = "0.2"
+VERSION = "0.3"
 
 
 def main(skip=False):
-    # skip if called from another function
-    if not skip:
-        create_config()
-        argument()
+    create_config()
+    argument()
     showSplash()
     sub, limit, order, nsfw = getInput()
     R3dditScrapper(sub, limit, order, nsfw=nsfw).start()
